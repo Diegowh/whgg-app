dd37c39e19b757a8840daabd7cf1c0f1af957a8b
diegowh <contacto.diegowh@gmail.com>
2023-12-13 19:59:47 +0100

    [Refactor] Remove the old folder/file structure

--- main.py
+++ main.py
@@ -1,50 +1,134 @@
-from flet import *
+import flet as ft
+import time
 
-from components.SearchPage.header_bar import HeaderBar
-from components.SearchPage.search_bar import SearchBar
+class HomeView(ft.UserControl):
+    def __init__(
+        self,
+        page: ft.Page,
+        route="/",
+        icon=ft.icons.HOME,
+        route_to="/home",
+    ):
+        super().__init__()
+        
+        self.page = page
+        self.icon = icon
+        self.route_to = route_to
 
-def main(page: Page):
-    BG = '#17171a'
-
-    
-    # Header bar
-    header_bar = HeaderBar()
-    
-    header_container = Container(
-        padding=padding.only(left=5, right=10),
-        bgcolor="#363062",
-        content=header_bar,
-    )
-    # Dropdown
-    dropdown = header_bar.get_server_dropdown()
-    
-    
-    header_container.alignment = alignment.center
-    
-    # Search bar
-    search_bar = SearchBar(dropdown)
-    search_bar_container = Container(
-        margin=margin.only(top=30),
-        bgcolor="#363062",
-        content=search_bar
-    )
-    text_field = search_bar.get_user_input()
-    
-    
-    # Bottom navigation bar
-    navigation_bar = NavigationBar(
-        height=90,
-        destinations=[
-            
-            NavigationDestination(icon=icons.HOME, label="Inicio"),
-            NavigationDestination(icon=icons.PEOPLE, label="Campeones"),
-            NavigationDestination(icon=icons.FORMAT_LIST_BULLETED, label="Tier List"),
-            NavigationDestination(icon=icons.SETTINGS, label="Settings")
+        self.controls = [
+            ft.SafeArea(
+                minimum=5,
+                content=ft.Column(
+                    controls = [
+                        
+                        # Header Bar
+                        ft.Row(
+                            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                            controls=[
+                                
+                                # LOGO WHGG
+                                ft.Text(
+                                    value="WHGG",
+                                    color="#e9665a",
+                                    size=60,
+                                    weight=ft.FontWeight.BOLD,
+                                ),
+                                
+                                # Server Dropdown
+                                ft.Container(
+                                    padding=ft.padding.only(left=10),
+                                    border_radius=8,
+                                    bgcolor="#e9665a",
+                                    content=ft.Dropdown(
+                                        text_size=16,
+                                        color=ft.colors.WHITE,
+                                        border="NONE",
+                                        border_radius=35,
+                                        width=80,
+                                        height=50,
+                                        hint_text="EUW",
+                                        options=[
+                                            ft.dropdown.Option("EUW"),
+                                            ft.dropdown.Option("NA"),
+                                            ft.dropdown.Option("RU"),
+                                            ft.dropdown.Option("EUNE"),
+                                            ft.dropdown.Option("TR"),
+                                            ft.dropdown.Option("BR"),
+                                            ft.dropdown.Option("LAN"),
+                                            ft.dropdown.Option("LAS"),
+                                            ft.dropdown.Option("OCE"),
+                                        ],
+                                    ),
+                                ),
+                            ]
+                        ),
+                        
+                        # Empty space
+                        ft.Divider(
+                            height=70,
+                            color="transparent",
+                        ),
+                        
+                        # Search Bar
+                        ft.Row(
+                            alignment=ft.MainAxisAlignment.SPACE_AROUND,
+                            controls=[
+                                
+                                # Summoner name input
+                                ft.TextField(
+                                    autocorrect=False,
+                                    autofocus=False,
+                                    label="Summoner name",
+                                    border=ft.InputBorder.UNDERLINE,
+                                    border_radius=10,
+                                ),
+                                
+                                # Search button
+                                ft.IconButton(
+                                    icon=ft.icons.SEARCH,
+                                    on_click=self.routing,
+                                )
+                            ]
+                        )
+                        
+                    ]
+                )
+            )
         ]
-    )
-    # Page settings
-    page.bgcolor = "#363062"
-    page.navigation_bar = navigation_bar
-    page.add(SafeArea(header_container), SafeArea(search_bar_container))
 
-app(target=main)
+    def build(self):
+        return self.controls
+    
+    
+    def routing(self):
+        time.sleep(1.9) # Para evitar la carga prematura de los controles
+        self.page.go(self.route_to)
+
+
+def main(page: ft.Page):
+    
+    page.theme_mode = ft.ThemeMode.DARK
+    
+    theme = ft.Theme()
+    theme.page_transitions.ios = ft.PageTransitionTheme.NONE
+    theme.page_transitions.macos = ft.PageTransitionTheme.NONE
+    page.theme = theme
+    
+    home = HomeView(page)
+    # profile = ProfileView(page)
+    
+    # def change_route(route):
+    #     page.views.clear()
+        
+    #     if page.route == "/":
+    #         page.views.append(home)
+        
+    #     if page.route == "/profile":
+    #         page.views.append(profile)
+            
+    #     page.update()
+    
+    page.add(home)
+    
+
+ft.app(target=main)
