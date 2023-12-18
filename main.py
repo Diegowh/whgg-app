77ef15ab11ff376bfaaad619fd2eab7c855355e9
diegowh <contacto.diegowh@gmail.com>
2023-12-18 16:36:00 +0100

    Update dropdown.Options

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import flet as ft
 import time
+import requests
 
 class HomeView(ft.UserControl):
     def __init__(
@@ -15,6 +16,8 @@ class HomeView(ft.UserControl):
         self.icon = icon
         self.route_to = route_to
 
+        self._response = None
+        
         self.summoner_name_textfield = ft.TextField(
             autocorrect=False,
             autofocus=False,
@@ -32,15 +35,21 @@ class HomeView(ft.UserControl):
             height=50,
             hint_text="EUW",
             options=[
-                ft.dropdown.Option("EUW"),
-                ft.dropdown.Option("NA"),
-                ft.dropdown.Option("RU"),
-                ft.dropdown.Option("EUNE"),
-                ft.dropdown.Option("TR"),
-                ft.dropdown.Option("BR"),
-                ft.dropdown.Option("LAN"),
-                ft.dropdown.Option("LAS"),
-                ft.dropdown.Option("OCE"),
+                ft.dropdown.Option(key="euw1", text="EUW"),
+                ft.dropdown.Option(key="eun1", text="EUNE"),
+                ft.dropdown.Option(key="na1", text="NA"),
+                ft.dropdown.Option(key="br1", text="BR"),
+                ft.dropdown.Option(key="jp1", text="JP"),
+                ft.dropdown.Option(key="kr", text="KR"),
+                ft.dropdown.Option(key="la1", text="LAN"),
+                ft.dropdown.Option(key="la2", text="LAS"),
+                ft.dropdown.Option(key="tr1", text="TR"),
+                ft.dropdown.Option(key="oc1", text="OCE"),
+                ft.dropdown.Option(key="ru", text="RU"),
+                ft.dropdown.Option(key="sg2", text="SG"),
+                ft.dropdown.Option(key="th2", text="TH"),
+                ft.dropdown.Option(key="vn2", text="VN"),
+                ft.dropdown.Option(key="tw2", text="TW"),
             ],
         )
         
@@ -100,32 +109,59 @@ class HomeView(ft.UserControl):
             )
         ]
 
+    @property
+    def response(self):
+        return self._response
+    
+    @response.setter
+    def response(self, value):
+        self._response = value
+        
+        
     def build(self):
         return self.controls
     
     def routing(self, event):
         time.sleep(0.3) # Para evitar la carga prematura de los controles
         
-        self.request(
-            summoner_name=self.summoner_name_textfield.text,
-            server=self.server_dropdown.text,
+        # Formatear el nombre de invocador para la solicitud
+        game_name, tagline = self.filter_textfield(value=self.summoner_name_textfield.value)
+        server = self.filter_dropdown_value(value=self.server_dropdown.value)
+        
+        self.response = self.request(
+            game_name=game_name,
+            tagline=tagline,
+            server=server,
         )
+        
         self.page.go(self.route_to)
     
-    def filter_tagline(self, sum_name):
+    def filter_dropdown_value(self, value: str = None):
+        if value is None:
+            return "euw1"
         
-        if "#" in sum_name:
-            game_name = sum_name.split("#")[0]
-            tagline = sum_name.split("#")[1]
+        return value
+    
+    def filter_textfield(self, value: str):
+        
+        if "#" in value:
+            game_name = value.split("#")[0]
+            tagline = value.split("#")[1]
         
         else:
-            game_name = sum_name
+            game_name = value
             tagline = "EUW"
         
-        # TODO Finalizar el método
+        return game_name, tagline
         
-    def request(self, summoner_name: str, server: str = "EUW"):
-        ...
+    def request(self, game_name: str, tagline: str, server: str = "EUW"):
+        print("Requesting...")
+        
+        response = requests.get(url=f"http://127.0.0.1:8000/api/{server}/{game_name}-{tagline}")
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Response: {response.status_code}. Something went wrong.")
 
 
 class ProfileView(ft.UserControl):
@@ -135,12 +171,15 @@ class ProfileView(ft.UserControl):
         route="/profile",
         icon=ft.icons.HOME,
         route_to="/",
+        response=None,
     ):
         super().__init__()
         
         self.page = page
         self.icon = icon
         self.route_to = route_to
+        
+        self.response = response
 
         self.page.appbar = ft.AppBar(
         title=ft.Text("Profile"),
@@ -157,6 +196,9 @@ class ProfileView(ft.UserControl):
                         #     bgcolor=ft.colors.BLUE,
                         # ),
                         ft.Text(value="Profile", size=50),
+                        ft.Text(value=f"Summoner name: {self.response['summoner_data']['name']}"),
+                        ft.Text(value=f"Summoner level: {self.response['summoner_data']['summoner_level']}"),
+                        ft.Text(value=f"Soloq Rank: {self.response['ranked_stats_data_list'][0]['tier'] + '' + self.response['ranked_stats_data_list'][0]['rank'] if self.response['ranked_stats_data_list'][0]['queue_type'] == 'RANKED_SOLO_5x5' else response['ranked_stats_data_list'][1]['tier'] + '' + self.response['ranked_stats_data_list'][1]['rank']}"),
                         ft.ElevatedButton(text="Go home", on_click=self.routing),
                     ]
                 )
@@ -182,7 +224,6 @@ def main(page: ft.Page):
     page.theme = theme
     
     home = HomeView(page)
-    profile = ProfileView(page)
     
     def route_change(e: ft.RouteChangeEvent) -> None:
         page.views.clear()
@@ -195,6 +236,9 @@ def main(page: ft.Page):
             page.views.append(home)
         
         if page.route == "/profile":
+            
+            response = home.response
+            profile = ProfileView(page=page, response=response)
             page.views.append(profile)
             
         page.update()
