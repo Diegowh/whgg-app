aec060559d70507a83ef3719aa5140d603f2a150
diegowh <contacto.diegowh@gmail.com>
2024-01-12 21:25:02 +0100

    Update home_view.py

--- views/home_view.py
+++ views/home_view.py
@@ -31,33 +31,6 @@ class HomeView(ft.UserControl):
             border_radius=10,
         )
 
-        self.server_dropdown = ft.Dropdown(
-            text_size=16,
-            color=ft.colors.WHITE,
-            border="NONE",
-            border_radius=35,
-            width=80,
-            height=50,
-            hint_text="EUW",
-            options=[
-                ft.dropdown.Option(key="euw1", text="EUW"),
-                ft.dropdown.Option(key="eun1", text="EUNE"),
-                ft.dropdown.Option(key="na1", text="NA"),
-                ft.dropdown.Option(key="br1", text="BR"),
-                ft.dropdown.Option(key="jp1", text="JP"),
-                ft.dropdown.Option(key="kr", text="KR"),
-                ft.dropdown.Option(key="la1", text="LAN"),
-                ft.dropdown.Option(key="la2", text="LAS"),
-                ft.dropdown.Option(key="tr1", text="TR"),
-                ft.dropdown.Option(key="oc1", text="OCE"),
-                ft.dropdown.Option(key="ru", text="RU"),
-                ft.dropdown.Option(key="sg2", text="SG"),
-                ft.dropdown.Option(key="th2", text="TH"),
-                ft.dropdown.Option(key="vn2", text="VN"),
-                ft.dropdown.Option(key="tw2", text="TW"),
-            ],
-        )
-
         self.controls = [
             ft.SafeArea(
                 minimum=5,
